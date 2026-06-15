5e35a55832f0c7f32096542603eb73a6e64e10b1
Yash kumar <100041816+Yashkbb@users.noreply.github.com>
2026-06-16 00:10:56 +0530

    Update Thank-you.js

--- Thank-you.js
+++ Thank-you.js
@@ -1,7 +1,1 @@
-export async function onRequest(context) {
-  const html = await fetch('https://raw.githubusercontent.com/Yashkbb/Thankyou-page/main/thankyou.html');
-  const text = await html.text();
-  return new Response(text, {
-    headers: { 'Content-Type': 'text/html;charset=UTF-8', 'Cache-Control': 'no-store' }
-  });
-}
+00
